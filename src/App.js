afcdfc84128e02aa77938caaebcdbfdee51d229d
faizan khan <faizank9058@gmail.com>
2025-03-17 10:00:49 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [todolist, setodolist] = useState([])
+  let Savetodolist = (event) => {
+    let todoname = event.target.todoname.value;
+    if (!todolist.includes(todoname)) {
+      let finaldolist = [todoname, ...todolist]
+      setodolist(finaldolist)
+      event.target.todoname.value = ''; // Clear input field
+    }
+    else {
+      alert("Name already exist")
+    }
+    event.preventDefault()
+  }
+  let list = todolist.map((value, index) => {
+    return (
+      <ListItem value={value} key={index}
+        indexNumber={index} todolist={todolist} setodolist={setodolist}
+      />
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+
+      <form onSubmit={Savetodolist}>
+        <input type="text" name="todoname" required />
+        <button>+</button>
+      </form>
+
+      <div className="outerdiv">
+        <ul>
+          {list}
+        </ul>
+      </div>
     </div>
   );
 }
-
 export default App;
+function ListItem({ value, indexNumber, todolist, setodolist }) {
+  let [status, setStatus] = useState(false)
+  let deleteRow = () => {
+    let finallist = todolist.filter((v, i) => i !== indexNumber)
+    setodolist(finallist)
+  }
+  let checkStatus = () => {
+    setStatus(!status)
+  }
+  return (
+    <li className={(status) ? 'checktodolist' : ''} onClick={checkStatus}> {value} <button className='cross-btn' onClick={deleteRow}>&times;</button> </li>
+  )
+}
